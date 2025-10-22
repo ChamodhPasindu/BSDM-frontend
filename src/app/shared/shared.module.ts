31193001c9367370cafb6813f09c67bfcd824196
ChamodhPasindu <chamodhpasindu2000@gmail.com>
2025-10-22 10:44:36 +0530

    change icons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AdminSidebarComponent } from './admin-sidebar/admin-sidebar.component';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule,RouterModule ],
+  imports: [CommonModule, RouterModule, MatIconModule],
   declarations: [AdminSidebarComponent, AdminHeaderComponent],
   exports: [AdminSidebarComponent, AdminHeaderComponent],
 })
